ab0a7e86ca7b5ed5a8cd5c1ec329d6bc00307145
Donghua Zhang <donghua.zhang@compass.com>
2022-08-30 11:37:56 +0800

    Feat: integrated spreado

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
+import {useMemo} from 'react';
+import {SpreadoSetupProvider} from 'spreado';
+import {
+  SpreadoSetupForReduxSwr,
+} from 'spreado/for-redux-swr';
 import { Provider } from 'react-redux'
 import { useStore } from '../store'
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState)
+  const spreadoSetup = useMemo(() => new SpreadoSetupForReduxSwr({store}), [store]);
 
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <SpreadoSetupProvider setup={spreadoSetup}>
+        <Component {...pageProps} />
+      </SpreadoSetupProvider>
     </Provider>
   )
 }
